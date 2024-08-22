347016c2e6d16b5b2da4203cfa819033a3ac7541
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-08-22 15:46:42 +0530

    add changes in router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './Views/Home/Home';
 import About from './Views/About/About';
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,15 +12,15 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const router = createBrowserRouter([
   {
-    path:'/',
-    element:<Home/>
+    path: '/',
+    element: <Home />
   },
 
   {
-    path:'/About',
-    element:<About/>
+    path: '/About',
+    element: <About />
   }
 ])
 
-root.render(<RouterProvider router={router} /> )
+root.render(<RouterProvider router={router} />)
 
