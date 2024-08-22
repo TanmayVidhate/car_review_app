f1b5acb13c7c49207f8e8cef710590382c88a7db
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-08-22 15:44:04 +0530

    add config Ui in files

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./Cards.css";
+import { THEME } from "../../config/data";
 const Cards=({imgurl,name,desc})=>{
     return(
         <>
             <div className="cards">
                 <img src={imgurl} alt="carimage" className="cardimg"/>
-                <p className="card_title">{name}</p>
+                <p className="card_title" style={{color:THEME.Highlight}}>{name}</p>
                 <p className="card_desc">{desc}</p>
                 <div className="btn-holder">
-                <button className="btn">Read More</button>
+                <button className="btn" style={{background:THEME.Secondary,color:THEME.Tertiary}}>Read More</button>
                 </div>
             </div>
         </>
